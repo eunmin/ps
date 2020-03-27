021e61f9bd5ec12b8643be3e5637ea3956337fee
Eunmin Kim <telepopsound@gmail.com>
2020-03-27 22:40:17 +0900

    [leetcode] 388. Longest Absolute File Path - Fix bug

--- src/main/java/leetcode/P388.java
+++ src/main/java/leetcode/P388.java
@@ -1,60 +1,68 @@
 package leetcode;
 
-public class P388 {
-    int max = 0;
-    int i = 0;
-    public void parse(int parentLen, int level, String s) {
-        int bufferLen = 0;
-        boolean isFile = false;
-        while(i < s.length()) {
-            if (s.charAt(i) == '\n') {
-                int currentPathLen = parentLen + bufferLen;
-                if (parentLen > 0) {
-                    currentPathLen++;
-                }
-                if (isFile) {
-                    if (currentPathLen > max) {
-                        max = currentPathLen;
-                    }
-                }
-                i++;
-                int depth = 0;
-                while (s.charAt(i) == '\t')  {
-                    depth++;
-                    i++;
-                }
-                if (depth == level) {
-                    bufferLen = 0;
-                    isFile = false;
-                } else if (depth == level + 1) {
-                    if (isFile) {
-                        throw new IllegalStateException();
-                    }
-                    parse(currentPathLen, level + 1, s);
-                } else if (depth == level - 1) {
+import java.util.HashMap;
+import java.util.Map;
 
-                    return;
-                } else {
-                    throw new IllegalStateException();
+public class P388 {
+
+    public int lengthLongestPath(String input) {
+        Map<Integer, Integer> lengths = new HashMap<>();
+        int i = 0;
+        int max = 0;
+        int depth = 0, length = 0;
+        boolean isFile = false;
+        while(i < input.length()) {
+            char c = input.charAt(i);
+            if (c == '\n' || i == input.length() - 1) {
+                if (i == input.length() - 1) length++;
+
+                lengths.put(depth, length);
+                if (isFile) {
+                    int sum = depth;
+                    for (int key : lengths.keySet()) {
+                        if (key <= depth) {
+                            sum += lengths.get(key);
+                        }
+                    }
+                    if (sum > max) {
+                        max = sum;
+                    }
                 }
+                depth = 0;
+                length = 0;
+                isFile = false;
+            } else if (c == '.') {
+                isFile = true;
+                length++;
+            } else if (c == '\t') {
+                depth++;
             } else {
-                if (s.charAt(i) == '.') {
-                    isFile = true;
-                }
-                bufferLen++;
-                i++;
+                length++;
             }
+            i++;
         }
-        int currentPathLen = parentLen + bufferLen + 1;
-        if (isFile) {
-            if (currentPathLen > max) {
-                max = currentPathLen;
-            }
-        }
+        return max;
     }
+
     public static void main(String[] args) {
-        P388 p = new P388();
-        p.parse(0,0,"dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext");
-        System.out.println(p.max);
+        String s0 =
+                "dir\n" +            // 3
+                "\tsubdir1\n" +      // 7
+                "\t\tfile1.ext\n" +  // 9
+                "\t\tsubsubdir1\n" + // 10
+                "\tsubdir2\n" +      // 7
+                "\t\tsubsubdir2\n" + // 10
+                "\t\t\tfile2.ext";   // 9
+
+        String s1 = "a.txt";
+        String s2 =
+                "a\n" +
+                "\taa\n" +
+                "\t\taaa\n" +
+                "\t\t\tfile1.txt\n" +
+                "aaaaaaaaaaaaaaaaaaaaa\n" +
+                "\tsth.png";
+
+        System.out.println(new P388().lengthLongestPath(s2));
     }
 }
