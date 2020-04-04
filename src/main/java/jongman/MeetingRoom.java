34950290db7fbd2164a33c7527b0413446e5b8b4
Eunmin Kim <telepopsound@gmail.com>
2020-04-04 23:38:07 +0900

    [jongman] MEETINGROOM

---
+++ src/main/java/jongman/MeetingRoom.java
@@ -0,0 +1,75 @@
+package jongman;
+
+import java.util.*;
+
+public class MeetingRoom {
+
+    public static class Pair {
+        int first;
+        int second;
+        public Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+    public boolean disjoint(Pair a, Pair b) {
+        return a.second <= b.first || b.second <= a.first;
+    }
+    public List<List<Integer>> makeGraph(List<Pair> meetings) {
+        List<List<Integer>> adj = new ArrayList<>();
+        int vars = meetings.size();
+        for (int i = 0; i <vars*2; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int i = 0; i <vars; i += 2) {
+            int j = i + 1;
+            adj.get(i * 2 + 1).add(j * 2);
+            adj.get(j * 2 + 1).add(i * 2);
+        }
+        for(int i = 0; i < vars; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if(!disjoint(meetings.get(i), meetings.get(j))) {
+                    adj.get(i * 2).add(j * 2 + 1);
+                    adj.get(j * 2).add(i * 2 + 1);
+                }
+            }
+        }
+        return adj;
+    }
+    public List<Boolean> solve2SAT(List<List<Integer>> adj) {
+        int n = adj.size() / 2;
+        List<Integer> label = tarjanSCC();
+        for (int i = 0; i < 2 * n; i += 2) {
+            if (label.get(i) == label.get(i + 1)) return new ArrayList<>();
+        }
+
+        List<Boolean> value = new ArrayList<>();
+        for (int i = 0; i< 2 * n; i++) {
+            value.add(null);
+        }
+        List<Pair> order = new ArrayList<>();
+        for (int i = 0; i < 2 * n; i++) {
+            order.add(new Pair(-1 * label.get(i), i));
+        }
+        // Collections.sort();
+        for (int i = 0; i < 2 * n; ++i) {
+            int vertex = order.get(i).second;
+            int variable = vertex / 2;
+            boolean isTrue = vertex % 2 == 0;
+            if (value.get(variable) != null) continue;
+            value.set(variable, !isTrue);
+        }
+        return value;
+    }
+    public static void main(String[] args) {
+        List<Pair> meetings = new ArrayList<>();
+        meetings.add(new Pair(2, 5));
+        meetings.add(new Pair(6, 9));
+        meetings.add(new Pair(1, 3));
+        meetings.add(new Pair(8, 10));
+        meetings.add(new Pair(4, 7));
+        meetings.add(new Pair(11, 12));
+        List<List<Integer>> adj = new MeetingRoom().makeGraph(meetings);
+        System.out.println(new MeetingRoom().solve2SAT(adj));
+    }
+}
